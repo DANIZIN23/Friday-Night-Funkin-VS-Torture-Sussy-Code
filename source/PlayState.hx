348f7b45abd4d91a6af1639b7fd858cc37c24f8b
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-12-02 12:40:27 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -835,6 +835,12 @@ class PlayState extends MusicBeatState
 		scoreTxt.cameras = [camHUD];
 		doof.cameras = [camHUD];
 
+		#if android
+		addAndroidControls();
+		androidc.visible = true;		
+		#end		
+		
+		
 		// if (SONG.song == 'South')
 		// FlxG.camera.alpha = 0.7;
 		// UI_camera.zoom = 1;
